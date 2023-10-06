152dc6981485a17b2c87d86c28bc002a94104cc2
refrantz <renan262001@gmail.com>
2023-10-06 15:58:23 -0300

    coronariesUXNet3D.py now uses command prompt. Previous Array script is now coronariesUXNet3DArray.py

--- coronariesUXNet3DArray.py
+++ coronariesUXNet3DArray.py
@@ -7,29 +7,7 @@ from monai.inferers import sliding_window_inference
 
 # Assuming the 3D-UXNet model is correctly imported as UXNET.
 # You may need to adjust the import statement based on your project structure.
-from network_backbone import UXNET 
-
-import os
-import sys
-from argparse import ArgumentParser
-
-
-# parse arguments
-parser = ArgumentParser(description="coronariesUXNet3D", epilog='\n')
-
-# input/outputs
-parser.add_argument("--i", help="Image(s) to segment. Can be a path to an image or to a folder.")
-parser.add_argument("--o", help="Segmentation output(s). Must be a folder if --i designates a folder.")
-parser.add_argument("--cpu", action="store_true", help="(optional) Enforce running with CPU rather than GPU.")
-
-# check for no arguments
-if len(sys.argv) < 2:
-    parser.print_help()
-    sys.exit(1)
-
-# parse commandline
-args = vars(parser.parse_args())
-
+from network_backbone import UXNET  
 
 val_transforms = Compose(
     [
@@ -45,17 +23,22 @@ val_transforms = Compose(
 )
 
 # Function to run inference and display the results
-def inference():
+def python_api(image_array=None, image_path=None, segmentation_path=None, roi_size=(96, 96, 96), sw_batch_size=2):
 
-    roi_size=(96, 96, 96)
-    sw_batch_size=2
-    original_nifti = nib.load(args['i'])
-    image = nib.load(args['i']).get_fdata()
+    if(image_path != None):
+        original_nifti = nib.load(image_path)
+        image = nib.load(image_path).get_fdata()
+    elif image_array != None:
+        image = image_array
+    else:
+        print("requires array or image path!")
+        return None
 
     data = {"image": image}
     transformed_data = val_transforms(data)
     input_tensor = transformed_data["image"]
     input_batch = input_tensor.unsqueeze(0)
+
         
     model = UXNET(
         in_chans=1,
@@ -68,12 +51,12 @@ def inference():
     )
 
     # Check if CUDA is available and move the model and input to GPU if possible
-    if torch.cuda.is_available() and not args['cpu']:
+    if torch.cuda.is_available():
         print("CUDA")
         model.to('cuda:0')
         model.load_state_dict(torch.load('./best_metric_model_2500.pth'))
         input_batch = input_batch.to('cuda:0')
-    elif torch.backends.mps.is_available() and not args['cpu']:
+    elif torch.backends.mps.is_available():
         print("MPS not Supported for 3D convolutions! Using CPU")
         model.load_state_dict(torch.load('./best_metric_model_2500.pth', map_location=torch.device("cpu")))
     else:
@@ -89,10 +72,12 @@ def inference():
     output = output.argmax(dim=1).squeeze(0).cpu().numpy().astype(np.uint8)
     nifti_image = nib.Nifti1Image(output, original_nifti.affine, original_nifti.header)
 
-    if args['o'] != None:
-        nib.save(nifti_image, args['o'])
+    if segmentation_path != None:
+        nib.save(nifti_image, "./label_" + segmentation_path)
 
     return output
 
-if __name__ == "__main__":
-    inference()
+
+image_path = '10018985_0000.nii.gz'
+
+python_api(image_path=image_path)
